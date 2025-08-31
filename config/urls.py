fd230b6c083a743748472be1101605ff8287fd3f
amiran <amurikokalandia@gmail.com>
2025-08-31 14:47:40 +0400

    removed urls

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('products/', include('products.urls')),
-    path('cart/', include('cart.urls')),
-    path('orders/', include('orders.urls')),
     path('accounts/', include('users.urls')),
 ]
 
